ffb6f0b3f1c4a8ab154a7e8bff201b545bd1bac7
Jose Fremaint <jlbiglion@gmail.com>
2017-02-25 23:25:20 -0500

    new content

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-$(document).ready(function(){
 
 // init controller
 var controller = new ScrollMagic.Controller();
@@ -22,6 +21,8 @@ new ScrollMagic.Scene({triggerElement: ".project_1", offset: -150})
 					.setClassToggle(".project_5", "fade-in") // add class toggle
 					.addTo(controller);
 
+$(document).ready(function(){
+
 $('.about-me a').click(function(){
 	$('.modal.jota').fadeIn();
 	$('body').css('overflow-y', 'hidden');
@@ -35,22 +36,54 @@ $('.portfolio-wrapper.project_2').click(function(){
 	$('.modal.mindful').fadeIn();
 	$('body').css('overflow-y', 'hidden');
 });
+$('.portfolio-wrapper.project_3').click(function(){
+	$('.modal.grimaldi').fadeIn();
+	$('body').css('overflow-y', 'hidden');
+});
+$('.portfolio-wrapper.project_4').click(function(){
+	$('.modal.pama').fadeIn();
+	$('body').css('overflow-y', 'hidden');
+});
+$('.portfolio-wrapper.project_5').click(function(){
+	$('.modal.physmedi').fadeIn();
+	$('body').css('overflow-y', 'hidden');
+});
 
 $('.close-btn').click(function(){
 	$('.modal').fadeOut();
 	$('body').css('overflow-y', 'visible');
 });
 
-$( ".portfolio-wrapper" ).hover(
+$( ".portfolio .portfolio-wrapper" ).hover(
   function() {
     $( this ).css({
 			'background-color': '#ffffff',
 			'cursor': 'pointer'
 		});
 		$(this).find('.portfolio-image-wrapper img').css('padding', '0px');
+		$(this).find('.project-title').css('margin-bottom', '35px');
   }, function() {
-    $( '.portfolio-wrapper, .portfolio-image-wrapper img' ).removeAttr( 'style' );
+    $( '.portfolio .portfolio-wrapper, .portfolio .portfolio-image-wrapper img, .project-title' ).removeAttr( 'style' );
   }
 );
+
+$( ".small-portfolio .portfolio-image-wrapper" ).hover(
+  function() {
+    $( this ).css({
+			'cursor': 'pointer'
+		});
+		$(this).find('img').css('padding', '0px');
+		$(this).find('p').css('padding', '40px 10px')
+  }, function() {
+    $(this).find('img, p').removeAttr( 'style' );
+  }
+);
+
+
+
+
+$(".social-icons a[href^='https://'], .social-icons a[href^='http://'] ").attr("target","_blank");
+
+
 
 });
